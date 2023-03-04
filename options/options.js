ce12808fcb64d80c5a8bada625364c9b86f4ebc4
Crowdbotics <engineering@crowdbotics.com>
2023-03-04 00:15:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-mouse-dev-67761",
+  url: "https://wandering-mouse-dev-67761.botics.co",
+  api: "https://wandering-mouse-dev-67761.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
